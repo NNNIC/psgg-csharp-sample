bfa77dcecdbdb88f67481c96a8c46130456ad212
nn4343 <gea01441@nifty.com>
2019-10-29 21:36:16 +0900

    created.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # psgg-csharp-sample
 StateGo C# Sample
+
+## Build Environment
+
+Windows 10
+Microsoft Visual Studio Community 2017 
+
+## This sample was using StateGo Beta7(Nov 2019 Release).
---
+++ m1/sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var sm = new TestControl();
+            sm.Run();
+        }
+    }
+}
